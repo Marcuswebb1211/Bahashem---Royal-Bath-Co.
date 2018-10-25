d65c1512895aa9864a2846a5111d23ddf7733650
Marcus Webb <marcuswebb1211@gmail.com>
2018-10-24 22:21:08 -0400

    add all necessary files

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+const modal = document.querySelector('.modal');
+const header = document.querySelector('header');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.menu-on').on("click", function() {
+
+        $('.modal').show();
+
+}); // end event handler
+
+
+
+$('.menu-off').on("click", function() {
+
+    $('.modal').hide();
+
+}); // end event handler
